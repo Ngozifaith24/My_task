04cd03f2f552a7500aaf4c8bab1f79d1af13f887
Ngozi Faith Omuekwu <faithblessing.2406@gmail.com>
2025-08-12 07:08:22 -0700

    added task2

--- Week_2/task2.py
+++ Week_2/task2.py
@@ -19,33 +19,33 @@
 # text = "Hello World"
 # print(text.replace("World", "Python"))
 
-# #No 6
-# text = "Python is a language"
-# print("Python"in text)
-# print("Java"in text)
+#No 6
+text = "Python is a language"
+print("Python"in text)
+print("Java"in text)
 
 #No. 7 
 
 
-# #No. 8
-# text = "Abuja    "
-# print(text.rstrip())
+#No. 8
+text = "Abuja    "
+print(text.rstrip())
 
-# text = "            Abuja"
-# print(text.lstrip())
+text = "            Abuja"
+print(text.lstrip())
 
-# #No 9
-# vowel_text = input("Enter your sentence: ")
+#No 9
+vowel_text = input("Enter your sentence: ")
 # text = vowel_text.lower()
-# print("number of vowel", text.count("a") +text.count("e") + text.count("i") +text.count("o")+ text.count("u") )
+print("number of vowel", text.count("a") +text.count("e") + text.count("i") +text.count("o")+ text.count("u") )
 
-# #No 10
-# num = "1234"
-# number = int(num)
-# print(number*2)
+#No 10
+num = "1234"
+number = int(num)
+print(number*2)
 
-# #11
-# friuts= "apple,banana,orange"
-# print(friuts.split())
+#11
+friuts= "apple,banana,orange"
+print(friuts.split())
 
 #12
